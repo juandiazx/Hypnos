3b2821cf2a7913cfcefbf9f359035054d81837fb
Alex <alexeltren@gmail.com>
2023-10-15 23:30:42 +0200

    Pantalla Principal

--- android/app/src/main/java/com/example/hypnosapp/MainActivity.java
+++ android/app/src/main/java/com/example/hypnosapp/MainActivity.java
@@ -1,8 +1,8 @@
 package com.example.hypnosapp;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +10,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         //setContentView(R.layout.activity_main);
-        setContentView(R.layout.preinicio_de_sesion);
+        setContentView(R.layout.activity_main);
     }
 }
