41a25c91606a3fa319d2caf53c91dd64b0ae0fa1
Elena <esowesp@epsg.upv.es>
2023-10-24 00:57:45 +0200

    cerrar sesion funciona bien

--- android/app/src/main/java/com/example/hypnosapp/PerfilUsuarioActivity.java
+++ android/app/src/main/java/com/example/hypnosapp/PerfilUsuarioActivity.java
@@ -1,13 +1,26 @@
 package com.example.hypnosapp;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class PerfilUsuarioActivity extends AppCompatActivity {
 
+    FirebaseAuth firebaseAuth;
+    FirebaseUser firebaseUser;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -50,8 +63,48 @@ public class PerfilUsuarioActivity extends AppCompatActivity {
         });
 
 
+        //obtenemos la sesión y el usuario:
+        firebaseAuth = FirebaseAuth.getInstance();
+        firebaseUser = firebaseAuth.getCurrentUser();
+
+        //funcionalidad del botón de cerrar sesión:
+        Button botonCerrarSesion = findViewById(R.id.btnCerrarSesion);
+        botonCerrarSesion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                cerrarSesion(v);
+            }
+        });
+
+
+        String nombreUsuario = firebaseUser.getDisplayName();
+        String correoUsuario = firebaseUser.getEmail();
+        //String passUsuario;
+        Uri urlFoto = firebaseUser.getPhotoUrl();
+
+        TextView nombre = findViewById(R.id.inputNombre);
+        nombre.setText(nombreUsuario);
+
+        TextView correo = findViewById(R.id.inputEmail);
+        correo.setText(correoUsuario);
+        /*
+        TextView contrasenya = findViewById(R.id.inputContrasenya);
+        contrasenya.setText(passUsuario);
+        */
+
     }
 
+    public void cerrarSesion(View view){
 
+        firebaseAuth.signOut();
+        Toast.makeText(this, "Se ha cerrado la sesión", Toast.LENGTH_SHORT).show();
 
+        //después de cerrar sesión nos dirigirá a la pantalla de pre-inicio de sesión:
+        Intent i = new Intent(getApplicationContext(), PreinicioDeSesion.class);
+        i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP
+                | Intent.FLAG_ACTIVITY_NEW_TASK
+                | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(i);
+        finish();
+    }
 }
